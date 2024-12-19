97ae03896cc34ff84624aba1ac33633aafb1b88a
Sam Wilson <el20sw@leeds.ac.uk>
2024-12-19 14:08:37 +0000

    revised data sanitation - bad variables identified and removed

--- PreProcessing.m
+++ PreProcessing.m
@@ -28,13 +28,33 @@ end
 %%
 %[text] ## Removing Bad Columns from the Data
 %[text] What counts as a bad columns?
-%[text] - Zero Variance \- the column is consistently the same value, this suggests that something is wrong with it \
+%[text] - Zero Variance \- the column is consistently the same value, this suggests that something is wrong with it
+%[text] - Those with malformed column names \- we expect columns to be the timestamp (position name, i.e. Pelvis, Thigh\_L, etc.) and then the same but with a 1 to 9 suffix. Anything outside this pattern is to be discarded \
 %%
 for i = 1:length(dataStruct)
     data = dataStruct(i).Data;
     badColumns = var(data{:,:}) == 0;
     cleanData = data(:, ~badColumns);
     dataStruct(i).CleanData = cleanData;
+end
+%%
+% There should be 63 columns in the data -
+% if the column name is greater than 9, then it is not a valid column
+% find each column which is just the position and remove it
+for i = 1:length(dataStruct)
+    data = dataStruct(i).CleanData;
+    badColumns = true(1, width(data));
+    for col = 1:width(data)
+        colName = data.Properties.VariableNames{col};
+        % we get a match when the regex is correct - if the result is empty
+        % there was no match and the column is invalid, we inverse this to
+        % make it go to zero and from there it works
+        regexResult = isempty(regexp(colName, '^(Thigh|Shank|Foot|Pelvis)(_)?([L|R])?(_)?([1-9])?$', 'once'));
+        if regexResult
+            badColumns(col) = false;
+        end
+    end
+    dataStruct(i).CleanData = data(:, badColumns);
 end
 %%
 %[text] ## Handling NaN values
@@ -66,18 +86,22 @@ end
 %[text] ## Calculating the sample rate of the data
 %[text] We have timestamps for each reading which can be used to get the sample rate of the data. The timestamps are on every 10th column
 for i = 1:length(dataStruct)
-    rawData = dataStruct(i).CleanData;
-    timestamps = rawData(:, 1:10:end-10);
+    data = dataStruct(i).CleanData;
+    timestampCols = false(1, width(data));
+    for col = 1:width(data)
+        colName = data.Properties.VariableNames{col};
+        regexResult = ~isempty(regexp(colName, '^(Thigh|Shank|Foot|Pelvis)(_)?([L|R])?$', 'once'));
+        if regexResult
+            timestampCols(col) = true;
+        end
+    end
+    timestamps = data(:, timestampCols);
     time_diffs = diff(timestamps);
     avg_time_diff = mean(time_diffs, 'all');
     dataStruct(i).SampleTime = avg_time_diff.mean;
-end
-%%
-%[text] We have the sample rate for each dataset, so we can remove the timestamp columns as these are not required for the classification
-for i = 1:length(dataStruct)
-    rawData = dataStruct(i).CleanData;
-    timestampIdx = 1:10:width(rawData);
-    dataStruct(i).CleanData(:, timestampIdx) = [];
+    % remove the timestamp columns as these aren't required for
+    % classification
+    dataStruct(i).CleanData(:, timestampCols) = [];
 end
 %%
 %[text] ## Filtering the Data
@@ -85,10 +109,10 @@ end
 %[text] ### Moving average filter
 window_size = 5;
 for i = 1:length(dataStruct)
-    rawData = dataStruct(i).CleanData;
-    numericData = rawData{:, :};
+    data = dataStruct(i).CleanData;
+    numericData = data{:, :};
     data_filtered = movmean(numericData, window_size);
-    dataStruct(i).CleanData = array2table(data_filtered, 'VariableNames', rawData.Properties.VariableNames);
+    dataStruct(i).CleanData = array2table(data_filtered, 'VariableNames', data.Properties.VariableNames);
 end
 %%
 %[text] ## Labelling the Data
