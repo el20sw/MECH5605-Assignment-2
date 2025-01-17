f1bba1166f89a004c61159aae782e607b665f5c7
Sam Wilson <el20sw@leeds.ac.uk>
2025-01-17 13:27:11 +0000

    ANN implementation renovation - add grid search for hyperparams

--- ANN.m
+++ ANN.m
@@ -1,23 +1,6 @@
 load preprocessed_with_features.mat
 
-% ToDo:
-% - get average accuracy from leaving out each subject
-% - note that this requires modification for leaving out each subject
-% - do some feature selection by randomly sampling from features and
-% measuring accuracy?
-% - find the 15 most useful features and report on these for the ANN
-
 allFeatures = [];
-
-% what do we need to do?
-% for each feature row in each data file, it need the activity to be appended to the features and the person needs to be known
-% then we can split the data into training and testing sets - we can use leave-one-subject-out cross-validation
-
-% 1. get the features and labels
-% 2. split the data into training and testing sets
-% 3. define and train the neural network
-% 4. test the neural network
-% 5. confusion matrix
 
 uniqueSubjects = grp2idx(unique({dataStruct.Subject}));
 for i = 1:length(dataStruct)
@@ -32,13 +15,15 @@ for i = 1:length(dataStruct)
 end
 %%
 % split the data into training and testing sets
-% using leave-one-subject-out cross-validation
+% using leave one subject out for testing
 
 % first we need to convert the subjects into a numerical representation
 allFeatures.Subject = grp2idx(allFeatures.Subject);
 subject = randsample(uniqueSubjects, 1);
 testIdx = allFeatures.Subject == subject;
 trainIdx = ~testIdx;
+
+classNames = unique(allFeatures.Activity);
 
 % extract the targets and remove from the features
 allTargets = allFeatures.Activity;
@@ -48,86 +33,132 @@ allFeatures.Activity = [];
 headingNames = allFeatures.Properties.VariableNames;
 headingNames = headingNames(~ismember(headingNames, {'Subject', 'Activity'}));
 
-% convert allFeatures into a matrix
+% convert allFeatures into a matrix - for an ANN, rows are features and columns are samples
 allFeatures = table2array(allFeatures);
+allFeatures(:, end) = [];
 
-trainFeatures = allFeatures(trainIdx, 1:end-1)';
+trainFeatures = allFeatures(trainIdx, 1:end)';
 trainTargets = allTargets(trainIdx, :);
 trainTargets = onehotencode(categorical(trainTargets), 2)';
 
-testFeatures = allFeatures(testIdx, 1:end-1)';
+testFeatures = allFeatures(testIdx, 1:end)';
 testTargets = allTargets(testIdx, :);
 testTargets = onehotencode(categorical(testTargets), 2)';
 %%
-% define and train the neural network
-numClasses = length(unique(allTargets));
-numFeatures = size(trainFeatures, 2);
-
+% Baseline test model
 net = patternnet(10);
 net.divideParam.trainRatio = 85/100;
 net.divideParam.valRatio = 15/100;
 net.divideParam.testRatio = 0/100;
-
 net.trainParam.showWindow = 0;
 [net, tr] = train(net, trainFeatures, trainTargets);
-%%
+
 % test the neural network
 predictedTargets = net(testFeatures);
-testPerformance = perform(net, testTargets, predictedTargets);
 view(net);
 figure
 [c, cm] = confusion(testTargets, predictedTargets);
 accuracy = sum(diag(cm))/sum(cm(:));
-plotconfusion(testTargets, predictedTargets); % may want to convert numeric to actual labels here
+plotconfusion(testTargets, predictedTargets);
 figure
 plotperform(tr);
 disp(accuracy);
 %%
-% Brute Force Feature selection - create a struct with the feature name and the accuracy
-accuracies = struct('Feature', {}, 'Accuracy', {});
-for i = 1:size(trainFeatures, 1)
-    fprintf('Feature %d of %d\n', i, size(trainFeatures, 1));
+% Define hyperparameters to search over
+hiddenLayer1Size = 1:50:500;
+hiddenLayer2Size = 1:50:500;
+hiddenLayerDepth = [1, 2];
 
-    targets = trainTargets;
-    inputs = trainFeatures(i, :);
+hyperparameters = [];
+bestAccuracy = 0;
 
-    net = patternnet(10);
+gridSize = length(hiddenLayer1Size) + length(hiddenLayer1Size) * length(hiddenLayer2Size);
+count = 0;
+
+for i = 1:length(hiddenLayerDepth)
+    depth = hiddenLayerDepth(i);
+    for j = 1:length(hiddenLayer1Size)
+        if depth == 2
+            for k = 1:length(hiddenLayer2Size)
+                count = count + 1;
+                disp(['Training network ', num2str(count), ' of ', num2str(gridSize)]);
+                disp(['Hidden layer 1 size: ', num2str(hiddenLayer1Size(j)), ', Hidden layer 2 size: ', num2str(hiddenLayer2Size(k))]);
+                layers = [hiddenLayer1Size(j), hiddenLayer2Size(k)];
+                [net, tr] = trainnet(trainFeatures, trainTargets, layers);
+
+                % test the neural network
+                accuracy = testNet(net, testFeatures, testTargets);
+                if accuracy > bestAccuracy
+                    bestAccuracy = accuracy;
+                end
+                hyperparameters = [hyperparameters; hiddenLayer1Size(j), hiddenLayer2Size(k), depth, accuracy];
+            end
+        else
+            count = count + 1;
+            disp(['Training network ', num2str(count), ' of ', num2str(gridSize)]);
+            disp(['Hidden layer size: ', num2str(hiddenLayer1Size(j))]);
+            layers = [hiddenLayer1Size(j)];
+            [net, tr] = trainnet(trainFeatures, trainTargets, layers);
+
+            % test the neural network
+            accuracy = testNet(net, testFeatures, testTargets);
+            if accuracy > bestAccuracy
+                bestAccuracy = accuracy;
+            end
+            hyperparameters = [hyperparameters; hiddenLayer1Size(j), NaN, depth, accuracy];
+        end
+    end
+end
+
+function [net, tr] = trainnet(trainFeatures, trainTargets, layers)
+    net = patternnet(layers);
     net.divideParam.trainRatio = 85/100;
     net.divideParam.valRatio = 15/100;
     net.divideParam.testRatio = 0/100;
 
     net.trainParam.showWindow = 0;
-    [net, tr] = train(net, inputs, targets);
-
-    predictedTargets = net(testFeatures(i, :));
-    [c, cm] = confusion(testTargets, predictedTargets);
-
-    accuracy = sum(diag(cm))/sum(cm(:));
-    accuracies(i).Feature = headingNames{i};
-    accuracies(i).Accuracy = accuracy;
+    [net, tr] = train(net, trainFeatures, trainTargets);
 end
-%%
-% Try with standardised features
-% define and train the neural network
-trainFeatures = zscore(trainFeatures);
-testFeatures = zscore(testFeatures);
 
-net = patternnet(10);
+function accuracy = testNet(net, testFeatures, testTargets)
+    predictedTargets = net(testFeatures);
+    testPerformance = perform(net, testTargets, predictedTargets);
+    [c, cm] = confusion(testTargets, predictedTargets);
+    accuracy = sum(diag(cm))/sum(cm(:));
+    fprintf('Accuracy: %f\n', accuracy);
+end
+
+save('ann_hyperparameters.mat', 'hyperparameters');
+%%
+% Find the best hyperparameters
+load ann_hyperparameters.mat
+
+accuracies = hyperparameters(:, end);
+% accuracies = str2double(accuracies);
+[~, idx] = max(accuracies);
+
+bestHyperparameters = hyperparameters(idx, :);
+hiddenLayer1Size = bestHyperparameters(1);
+hiddenLayer2Size = bestHyperparameters(2);
+hiddenLayerDepth = bestHyperparameters(3);
+
+layers = [hiddenLayer1Size, hiddenLayer2Size];
+if isnan(hiddenLayer2Size)
+    layers = hiddenLayer1Size;
+end
+
+net = patternnet(layers);
 net.divideParam.trainRatio = 85/100;
 net.divideParam.valRatio = 15/100;
 net.divideParam.testRatio = 0/100;
-
 net.trainParam.showWindow = 0;
 [net, tr] = train(net, trainFeatures, trainTargets);
 
 % test the neural network
 predictedTargets = net(testFeatures);
-testPerformance = perform(net, testTargets, predictedTargets);
-view(net);
-figure
 [c, cm] = confusion(testTargets, predictedTargets);
 accuracy = sum(diag(cm))/sum(cm(:));
-plotconfusion(testTargets, predictedTargets); % may want to convert numeric to actual labels here
+plotconfusion(testTargets, predictedTargets);
 figure
 plotperform(tr);
 disp(accuracy);
